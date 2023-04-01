4fe04c709791389cbd5bdaed753bd8741e6c26b2
Firdosh01 <firdosh7392@gmail.com>
2023-04-01 00:22:58 -0500

    slider logic

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -2,21 +2,44 @@ import React from 'react'
 import Card from './Card'
 import {FaChevronLeft, FaChevronRight} from 'react-icons/fa'
 import  './Card.css'
+import { useState } from 'react'
 
 function Testimonials(props) {
     let  reviews = props.reviews;
+
+    const [index, setIndex] = useState(0);
+
+    function leftShiftHandler() {
+        if(index - 1 < 0) {
+            setIndex(reviews.length - 1);
+        }
+        else {
+            setIndex(index - 1);
+        }
+    }
+
+    function rightShiftHandler() {
+        if(index + 1 >=reviews.length) {
+            setIndex(0);
+        }
+        else {
+            setIndex(index+1);
+        }
+
+    }
+
 
   return (
     <div className='relative'>
       
       <Card review = {reviews[0]} /> 
 
-      <div className='left-icon'>
+      <div className='left-icon' onClick={leftShiftHandler}>
         < FaChevronLeft />
       </div>
       
 
-      <div className='right-icon' >
+      <div className='right-icon' onClick={rightShiftHandler}>
         < FaChevronRight />
       </div>
 
